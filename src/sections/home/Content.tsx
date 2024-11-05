bc39a3bda7ef9102baf5b14ccd9c7ec8a193a706
Borys Ivliev <boris.ivlev.11@gmail.com>
2024-11-05 18:54:42 +0100

    Add Animation to Home component
    Animated blocks such as content and image have been added to Home component

--- src/sections/home/Content.tsx
+++ src/sections/home/Content.tsx
@@ -1,21 +1,85 @@
 import { Github, Linkedin } from "lucide-react";
+import { motion } from "framer-motion";
+import { ReactElement } from "react";
+
+// Универсальные настройки для анимации
+const divVariants = {
+    hidden: {
+        opacity: 0,
+        x: -100,
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+    },
+};
+
+const btnVariants = {
+    hidden: {
+        opacity: 0,
+        scale: 0.8,
+        y: 100,
+    },
+    visible: {
+        opacity: 1,
+        scale: 1,
+        y: 0,
+    },
+};
+
+// Компонент AnimatedItem для анимации элементов
+const AnimatedItem = ({
+    children,
+    delay = 0,
+}: {
+    children: ReactElement;
+    delay: number;
+}) => (
+    <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={divVariants}
+        transition={{ duration: 0.5, delay }}
+    >
+        {children}
+    </motion.div>
+);
 
 const Content = () => {
     return (
         <div className="mt-8 text-white text-2xl sm:w-full lg:w-[40%] flex flex-col order-2 lg:order-1">
-            <p>Software Developer</p>
-            <p className="text-7xl leading-none mt-3">
-                Hello, I'm
-                <br />
-                <span className="text-accent">Borys Ivliev</span>
-            </p>
-            <p className="text-base mt-5 leading-7 w-2/3 lg:w-full mx-auto">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
-                cum veniam expedita deleniti optio temporibus quas. Quasi
-                architecto earum repellendus nostrum omnis eaque aspernatur
-                iure, possimus autem tenetur maxime reprehenderit.
-            </p>
-            <div className="flex mt-5 gap-5 items-center mx-auto lg:ml-0">
+            {/* Анимация для заголовка */}
+            <AnimatedItem delay={0}>
+                <p>Software Developer</p>
+            </AnimatedItem>
+
+            {/* Анимация для основного текста */}
+            <AnimatedItem delay={0.1}>
+                <p className="text-7xl leading-none mt-3">
+                    Hello, I'm
+                    <br />
+                    <span className="text-accent">Borys Ivliev</span>
+                </p>
+            </AnimatedItem>
+
+            {/* Анимация для описательного текста */}
+            <AnimatedItem delay={0.2}>
+                <p className="text-base mt-5 leading-7 w-2/3 lg:w-full mx-auto">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Enim cum veniam expedita deleniti optio temporibus quas.
+                    Quasi architecto earum repellendus nostrum omnis eaque
+                    aspernatur iure, possimus autem tenetur maxime
+                    reprehenderit.
+                </p>
+            </AnimatedItem>
+
+            <motion.div
+                className="flex mt-5 gap-5 items-center mx-auto lg:ml-0"
+                initial="hidden"
+                animate="visible"
+                variants={btnVariants}
+                transition={{ delay: 0.3, duration: 1 }}
+            >
                 <button className="p-3 border-accent border-2 rounded-full hover:bg-accent-hover transition-colors duration-300 text-sm text-accent hover:text-white">
                     DOWNLOAD CV
                 </button>
@@ -26,10 +90,8 @@ const Content = () => {
                     <a href="/" className="icon_link">
                         <Linkedin />
                     </a>
-                    <div></div>
-                    <div></div>
                 </div>
-            </div>
+            </motion.div>
         </div>
     );
 };
--- src/sections/home/Image.tsx
+++ src/sections/home/Image.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+import { motion } from "framer-motion";
+
+const imgVariants = {
+    initial: { opacity: 0 },
+    animate: {
+        opacity: 1,
+        transition: { duration: 1.5, ease: "easeInOut" },
+    },
+};
 
 const Image = () => {
     return (
@@ -6,10 +14,13 @@ const Image = () => {
             <div className="relative w-[500px] h-[500px] rounded-full flex items-center justify-center p-3">
                 <div className="absolute inset-0 border-2 border-accent rounded-full animate-pulse"></div>
 
-                <img
+                <motion.img
                     src="public/assets/developer.png"
                     alt="developer"
                     className="w-full h-full object-cover rounded-full mix-blend-lighten"
+                    initial="initial"
+                    animate="animate"
+                    variants={imgVariants}
                 />
             </div>
         </div>
