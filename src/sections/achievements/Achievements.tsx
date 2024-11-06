153f201dd0d77e3e20c7ca95d2f16c2bd9896748
Borys Ivliev <boris.ivlev.11@gmail.com>
2024-11-06 21:37:42 +0100

    Adaptability for all devices has been added to the Achievements component

--- src/sections/achievements/Achievements.tsx
+++ src/sections/achievements/Achievements.tsx
@@ -18,10 +18,10 @@ const divVariants = {
 
 const Achievements = () => {
     return (
-        <div className="mt-24 flex gap-20 mb-10 mx-auto w-fit">
+        <div className="mt-12 lg:mt-24 grid sm:grid-cols-2 lg:grid-cols-4 w-[80%] lg:w-full gap-3 xl:gap-5 lg:gap-15 mb-10 mx-auto justify-items-center">
             {achievements.map((el) => (
                 <motion.div
-                    className="text-white flex items-center w-[240px] border-accent text-center border-2 rounded-full px-5 py-3 gap-3"
+                    className="text-white flex items-center w-[230px] border-accent text-center border-2 rounded-full px-5 py-3 gap-3"
                     initial="hidden"
                     animate="visible"
                     custom={el.id}
